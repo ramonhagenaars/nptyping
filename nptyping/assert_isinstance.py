1ff8c5815c3a9984f94c5c0dea41ee2ab7c21fee
Ramon <p8u7wAPC5Pg9HYkkCkzA>
2022-03-25 22:39:09 +0100

    Added pragma no cover for import error not raised on python 3.10

--- nptyping/assert_isinstance.py
+++ nptyping/assert_isinstance.py
@@ -31,7 +31,7 @@ from typing import (
 
 try:
     from typing import TypeGuard
-except ImportError:
+except ImportError:  # pragma: no cover
     from typing_extensions import TypeGuard
 
 TYPE = TypeVar("TYPE")
