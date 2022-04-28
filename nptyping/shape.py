533c5a008545ee366841dd4326af136bc2516779
Ramon <p8u7wAPC5Pg9HYkkCkzA>
2022-04-28 22:54:36 +0200

    Added Shape class.

--- nptyping/shape.py
+++ nptyping/shape.py
@@ -21,71 +21,38 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from typing import (
-    Any,
-    Dict,
-    Set,
-    Tuple,
-)
+from abc import ABC
+from typing import Any, Dict
 
-from nptyping.error import InvalidArgumentsError
+from nptyping.nptyping_type import NPTypingType
+
+from nptyping.base_meta_classes import ContainerMeta
 from nptyping.shape_expression import (
     get_dimensions,
     normalize_shape_expression,
     remove_labels,
     validate_shape_expression,
 )
-from nptyping.subscriptable_meta import SubscriptableMeta
 
 
-class ShapeMeta(SubscriptableMeta, cls_name="Shape"):
+class ShapeMeta(ContainerMeta, name="Shape"):
     """
-    Metaclass that is coupled to nptyping.Shape. It contains all actual logic
-    such as instance checking.
+    Metaclass that is coupled to nptyping.Shape.
     """
 
-    _known_expressions: Set[str] = set()
-
-    def _get_item(cls, item: Any) -> Tuple[Any, ...]:
-        if not isinstance(item, str):
-            raise InvalidArgumentsError(
-                f"Unexpected argument of type" f" {type(item)}, expecting a string."
-            )
-
-        if item in cls._known_expressions:
-            # No need to do costly validations and normalizations if it has been done
-            # before.
-            return (item,)
-
+    def _validate_expression(cls, item: str) -> None:
         validate_shape_expression(item)
-        norm_shape_expression = normalize_shape_expression(item)
-        cls._known_expressions.add(norm_shape_expression)
-        return (norm_shape_expression,)
+
+    def _normalize_expression(cls, item: str) -> str:
+        return normalize_shape_expression(item)
 
     def _get_additional_values(cls, item: Any) -> Dict[str, Any]:
         dim_strings = get_dimensions(item)
         dim_string_without_labels = remove_labels(dim_strings)
         return {"prepared_args": dim_string_without_labels}
 
-    def __subclasscheck__(cls, subclass: Any) -> bool:
-        return type(  # pylint: disable=unidiomatic-typecheck
-            subclass
-        ) is ShapeMeta and (subclass.__args__ == cls.__args__ or not cls._parameterized)
 
-    def __eq__(cls, other: Any) -> bool:
-        return hasattr(other, "__args__") and other.__args__ == cls.__args__
-
-    def __hash__(cls) -> int:
-        return hash(cls.__args__)
-
-    def __str__(cls: "Shape") -> str:
-        return f"Shape['{cls.__args__[0]}']"
-
-    def __repr__(cls) -> str:
-        return str(cls)
-
-
-class Shape(metaclass=ShapeMeta):
+class Shape(NPTypingType, ABC, metaclass=ShapeMeta):
     """
     A container for shape expressions that describe the shape of an multi
     dimensional array.
