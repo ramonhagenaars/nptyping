f552268a5cbf6069b42576a264793f41b86a6dca
Ramon <p8u7wAPC5Pg9HYkkCkzA>
2022-09-10 20:38:18 +0200

    Added constraints per version

--- tasks.py
+++ tasks.py
@@ -48,6 +48,10 @@ def get_venv(py=None):
     return f".venv{py}" if py else _DEFAULT_VENV
 
 
+def get_constraints(py=None):
+    return f"constraints-{py}.txt" if py else "constraints.txt"
+
+
 def get_py(py=None):
     return f"{get_venv(py)}{_PY_SUFFIX}"
 
@@ -119,10 +123,11 @@ def venv(context, py=None):
 @task
 def lock(context, py=None):
     """Lock the project dependencies in a constraints file."""
-    print("Updating the constraints.txt")
-    context.run(
-        f"{get_py(py)} -m piptools compile ./dependencies/* --output-file constraints.txt --quiet"
-    )
+    for version in get_versions(py):
+        print_header(version, lock)
+        context.run(
+            f"{get_py(version)} -m piptools compile ./dependencies/* --output-file {get_constraints(version)} --quiet"
+        )
 
 
 @task
@@ -133,7 +138,9 @@ def install(context, py=None):
         print(f"Upgrading pip")
         context.run(f"{get_py(version)} -m pip install --upgrade pip")
         print(f"Installing dependencies into: {version}")
-        context.run(f"{get_pip(version)} install .[dev] --constraint constraints.txt")
+        context.run(
+            f"{get_pip(version)} install .[dev] --constraint {get_constraints(version)}"
+        )
 
 
 @task(clean, venv, lock, install)
