8c6fe86557234b0cbe3b0f6ce94c6011272a9ba5
Ramon <p8u7wAPC5Pg9HYkkCkzA>
2022-09-14 21:09:32 +0200

    Pointed the DataFrame stub to the pandas-stubs fork DataFrame.

--- nptyping/pandas_/dataframe.pyi
+++ nptyping/pandas_/dataframe.pyi
@@ -21,12 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from typing import (
-    Generic,
-    Protocol,
-    TypeVar,
-)
 
-T = TypeVar("T", covariant=True)
+import pandas as pd
 
-class DataFrame(Protocol, Generic[T]): ...  # FIXME
+DataFrame = pd.DataFrame
